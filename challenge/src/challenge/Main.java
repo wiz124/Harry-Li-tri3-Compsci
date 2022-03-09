770aa26a5eda511ca4760a9e77d5fac603d3126f
wiz124 <hli16492@gmail.com>
2022-03-09 14:40:22 -0800

    challenge 1 completed

--- challenge/src/challenge/Main.java
+++ challenge/src/challenge/Main.java
@@ -1,4 +1,5 @@
 package challenge;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
@@ -11,22 +12,20 @@ public class Main {
         //initialize necessary var
         boolean f= true;
        String selection;
-        Scanner scan =  new Scanner(System.in);
+
 
 
 
         //while loop to loop through menu, waiting for exit program
     //try catch block looped to make sure we get right response, otherwise quit program
         String[] options = new String[] {"0)Quit", "1)Integer Swap", "2)2D Array Printer"};
-
+        Scanner scan =  new Scanner(System.in);
     while (f) {
-
-
 
         try {
             //wait time for loop, allow reader to read prompts and results
             TimeUnit.SECONDS.sleep(1);
-
+            scan = new Scanner(System.in);
             //prompt:
             System.out.println("\nMenu page: ");
             for(String i:options){
@@ -37,20 +36,25 @@ public class Main {
             System.out.println("Choose a program: ");
             selection = scan.nextLine();
 
-            //checking selection
-            //quit selection
+            //checking selections
+            //quit programs
             if (selection.equals( "0")) {
+                scan.close();
                 break;
             }
             //intswap selection
             else if (selection.equals( "1")) {
-                intswap object = new intswap();
-                object.main();
-                TimeUnit.SECONDS.sleep(3);
+                System.out.println("Enter an integer: ");
+                int a = scan.nextInt();
+                System.out.println("Enter an integer: ");
+                int b = scan.nextInt();
 
+                IntByReference.swapper(a,b);
+                TimeUnit.SECONDS.sleep(3);
             }
             //array printer selection
             else if (selection.equals( "2")) {
+
                 arrprint object2 = new arrprint();
                 object2.main();
                 TimeUnit.SECONDS.sleep(3);
@@ -60,19 +64,20 @@ public class Main {
             //create an error for try catch block to activate and be useful
           else {
                 String myString = null;
-                System.out.println(myString.length());
+             System.out.println(myString.length());
+               // System.out.println(options.length+1);
             }
 
 
+        } catch(Exception e){
+            System.out.println("Incorrect input");
+
         }
 
-        catch (Exception e) {
-            //reprompt
-            System.out.println("Incorrect Selection. Must pick number from menu.");
-        }
-        }
+    }
     System.out.println("Goodbye");
-    scan.close();
+        scan.close();
+
     }
 
 
