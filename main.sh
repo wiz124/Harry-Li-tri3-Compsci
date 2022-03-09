164bf3d13911c89fa6066c28cec1535edafad3db
wiz124 <hli16492@gmail.com>
2022-03-09 09:58:19 -0800

    challenge 1 completed

--- main.sh
+++
@@ -1,8 +0,0 @@
-javac *.java # The * just means to compile all the java files in the directory
-
-if [$? = 0] # $? just means the exit code of the last command. a command with exit code 0 means it was successful, we only want to run the java program if the program compiled successfully
-then
-  java Main
-else
-  printf "\n\nError - Exit code $?"
-fi
