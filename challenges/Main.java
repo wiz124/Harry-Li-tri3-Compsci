8e6c1df520c314fd7ae3cdc801e0b6115475618f
wiz124 <hli16492@gmail.com>
2022-04-08 10:08:20 -0700

    replit file

--- challenges/Main.java
+++ challenges/Main.java
@@ -13,7 +13,7 @@ public class Main {
     public static void main(String[] args) {
         // menu for replit, use try catch block in development
 
-        // initialize necessary var
+        // initialize necessary variable
         boolean f = true;
         String selection;
 
